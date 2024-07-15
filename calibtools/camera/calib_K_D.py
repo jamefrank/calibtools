689a8d136f3b3adff22352f718a35d5fddfe9d59
fanjin <jinfan.@novauto.com.cn>
2024-07-15 23:19:29 +0800

    [feat] finish charuco camera calib K&D

--- calibtools/camera/calib_K_D.py
+++ calibtools/camera/calib_K_D.py
@@ -2,7 +2,7 @@
 Author: fanjin jinfan.@novauto.com.cn
 Date: 2024-07-13 22:35:36
 LastEditors: fanjin jinfan.@novauto.com.cn
-LastEditTime: 2024-07-15 00:47:56
+LastEditTime: 2024-07-15 23:16:58
 FilePath: /calibtools/calibtools/camera/calib_K_D.py
 Description:  标定相机内参和畸变系数
 
@@ -14,6 +14,7 @@ import cv2
 import glob
 import yaml
 import sys
+import json
 import argparse
 import numpy as np
 import cv2.aruco as aruco
@@ -25,20 +26,27 @@ from calibtools.camera.common import CamModel
 logger = log_utils.get_logger()
 
 
-def calib_fish_camera_charuco():
+def calib_camera_charuco():
     parser = argparse.ArgumentParser(description='calib fish camera K&D use charuco', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('-i','--img_dir', type=str, help='image dir')
     parser.add_argument('-c', '--charuco_yaml_file', type=str, help='charuco yaml file')
     parser.add_argument('-o','--output_dir', type=str, help='output dir')
-    parser.add_argument('-v', '--visilize', action='store_true', help='visualize calib')
     parser.add_argument('-s', '--save_intermediate_result', action='store_true', help='save intermediate result')
+    parser.add_argument('-m', '--model', choices=['fisheye', 'pinhole'], type=str, help='camera model')
     
     params = parser.parse_args()
     img_dir = params.img_dir
     charuco_yaml_file = params.charuco_yaml_file
     output_dir = params.output_dir
-    enable_visilize = params.visilize
+    os.makedirs(output_dir, exist_ok=True)
+    os.makedirs(os.path.join(output_dir, 'temp'), exist_ok=True)
+    
     enable_save_intermediate_result = params.save_intermediate_result
+    cam_model = None
+    if "fisheye" == params.model:
+        cam_model = CamModel.FISH
+    elif "pinhole" == params.model:
+        cam_model = CamModel.PINHOLE
     
     if not os.path.exists(charuco_yaml_file):
         logger.error('charuco_yaml_file not exists')
@@ -109,46 +117,95 @@ def calib_fish_camera_charuco():
     
     
     #
-    flags = cv2.fisheye.CALIB_FIX_SKEW | cv2.fisheye.CALIB_FIX_PRINCIPAL_POINT  | cv2.fisheye.CALIB_RECOMPUTE_EXTRINSIC
-    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 100, 1e-9)
-    retval, K, D, rvecs, tvecs = cv2.fisheye.calibrate(all_obj_points, all_corners, image_size, None, None, flags=flags, criteria=criteria)
+    if cam_model == CamModel.FISH:
+        flags = cv2.fisheye.CALIB_FIX_SKEW | cv2.fisheye.CALIB_FIX_PRINCIPAL_POINT  | cv2.fisheye.CALIB_RECOMPUTE_EXTRINSIC
+        criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 100, 1e-9)
+        retval, K, D, rvecs, tvecs = cv2.fisheye.calibrate(all_obj_points, all_corners, image_size, None, None, flags=flags, criteria=criteria)
+    elif cam_model == CamModel.PINHOLE:
+        flags = cv2.CALIB_FIX_PRINCIPAL_POINT | cv2.CALIB_FIX_K4 | cv2.CALIB_FIX_K5 | cv2.CALIB_FIX_K6 | cv2.CALIB_FIX_S1_S2_S3_S4  | cv2.CALIB_FIX_TAUX_TAUY
+        criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 100, 1e-9)
+        retval, K, D, rvecs, tvecs = aruco.calibrateCameraCharuco(all_corners, all_ids, board, image_size, None, None, None, None, flags=flags, criteria=criteria)
     
-
+    #
+    calib_res = {}
+    calib_res['K'] = K.tolist()
+    calib_res['D'] = D.squeeze().tolist()
+    calib_res['W'] = image_size[1]
+    calib_res['H'] = image_size[0]
+    calib_res['Error'] = retval
+    calib_res['temp'] = []
+    
     #
     for i,obj_points in enumerate(all_obj_points):
         img = cv2.imread(all_paths[i])
         aruco.drawDetectedCornersCharuco(img, all_corners[i], all_ids[i])
-        img_points = cv2.fisheye.projectPoints(obj_points, rvecs[i], tvecs[i], K, D)[0]
+        if cam_model == CamModel.FISH:
+            img_points = cv2.fisheye.projectPoints(obj_points, rvecs[i], tvecs[i], K, D)[0]
+        else:
+            img_points = cv2.projectPoints(obj_points, rvecs[i], tvecs[i], K, D)[0]
         img_points = img_points.reshape(-1, 2)
         corners = all_corners[i].reshape(-1, 2)
         error = cv2.norm(img_points, corners, cv2.NORM_L2) / len(img_points)
         logger.info(f'{i}: {all_paths[i]}, error: {error}')
+        
+        cv2.putText(img, f'Error: {error:.2f}', (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
         for pt in img_points:
-            cv2.circle(img, (int(pt[0]),int(pt[1])), 5, (0, 255, 255), 1)
+            cv2.circle(img, (int(pt[0]),int(pt[1])), 5, (0, 0, 255), 1)
         if enable_save_intermediate_result:
-            cv2.imwrite(f'{output_dir}/{os.path.basename(all_paths[i])}.jpg', img)
+            cv2.imwrite(f'{output_dir}/temp/{os.path.basename(all_paths[i])}.jpg', img)
+            
+        calib_res['temp'].append(
+            {
+                'error': error,
+                'img_path': all_paths[i],
+                'rvec': rvecs[i].squeeze().tolist(),
+                'tvec': tvecs[i].squeeze().tolist(),
+                'corners': corners.tolist(),
+            }
+        )
+        
+    
+    with open(os.path.join(output_dir, 'K_D.json'), 'w') as f:
+        json.dump(calib_res, f)    
+    
+    logger.info(f'K:\n{K}')
+    logger.info(f'D:\n{D}')
     logger.info(f'Total Mean Error: {retval}')
-    pass
+    
 
     
 def main():
     
+    # # 模拟命令行参数
+    # args = [
+    #     '-i', '/home/frank/data/g3/calib_imgs/fisheyex4/front',
+    #     '-c', '/home/frank/data/github/calibtools/calibtools/camera/charuco.yaml',
+    #     '-o', '/home/frank/data/github/calibtools/data/calib_results/ffish',
+    #     '-s',
+    #     '-m', 'fisheye'
+    # ]
+    # # 替换 sys.argv 的值为模拟的参数列表
+    # sys.argv = ['script_name.py'] + args
     
-    output_dir = "/home/frank/data/github/calibtools/data/calib_results"
+    # # 调用函数
+    # calib_camera_charuco()
+    
+    
     
     # 模拟命令行参数
     args = [
-        '-i', '/home/frank/data/g3/calib_imgs/fisheyex4/front',
-        '-c', './calibtools/camera/charuco.yaml',
-        '-o', '/home/frank/data/github/calibtools/data/calib_results',
-        '-v',
-        '-s'
+        '-i', '/home/frank/data/g3/calib_imgs/front8m',
+        '-c', '/home/frank/data/github/calibtools/calibtools/camera/charuco.yaml',
+        '-o', '/home/frank/data/github/calibtools/data/calib_results/f800',
+        '-s',
+        '-m', 'pinhole'
     ]
     # 替换 sys.argv 的值为模拟的参数列表
     sys.argv = ['script_name.py'] + args
     
     # 调用函数
-    calib_fish_camera_charuco()
+    calib_camera_charuco()
+    
     
     pass
     
